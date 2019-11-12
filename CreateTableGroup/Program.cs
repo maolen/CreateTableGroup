10439e0a2c6f578b2de0d0505357f2f096faf366
Kamila <zhakupova.kamila@gmail.com>
2019-11-12 14:56:40 +0600

    Создала таблицу group

---
+++ CreateTableGroup/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Console;
+using System.Data.SqlClient;
+
+namespace CreateTableGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "Server=DESKTOP-NVDGPN3;Database=UniversityDb;Trusted_Connection=true;";
+            var query =
+            @"CREATE TABLE [dbo].[gruppa]
+                (
+                    [Id] [uniqueidentifier] NOT NULL PRIMARY KEY,
+                    [Name] [nvarchar](max) NULL
+                );";
+            
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand(query, connection);
+                try
+                {
+                    command.ExecuteNonQuery();
+                    WriteLine("Таблица успешно создана!");
+                }
+                catch (SqlException exception)
+                {
+                    WriteLine("Ошибка создания таблицы: " + exception.ToString());
+                }
+            }
+        }
+    }
+}
